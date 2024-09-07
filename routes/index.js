15eb4e7a5629be0b644f9fe567f763f49efd4f3a
nodepanel01 <nodepanel01@gmail.com>
2024-09-07 11:14:06 +0530

    Update index.js
    log name change

--- routes/index.js
+++ routes/index.js
@@ -470,7 +470,7 @@ router.get('/buySellApi', async function (req, res) {
                 "data": null
               });
             })
-            let html = '<b>Account Id : </b> Dhruti <b>[Bybit]</b> \n\n' +
+            let html = '<b>Account Id : </b> Vijay <b>[Bybit]</b> \n\n' +
             '🔀 <b>Direction : </b> <b> ' + req.query.transaction_type + '</b>'+(req.query.transaction_type == 'buy'? '🟢' : '🔴')+'\n' +
             '🌐 <b>Script : </b> ' + req.query.instrument_token + '\n' +
             '💰 <b>Price : ₹</b> ' + req.query.price + '\n' +
